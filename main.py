88244a99a9532fce2862cae0fcb6d4589707d280
Florijan Stamenkovic <florijan.stamenkovic@gmail.com>
2018-05-21 14:41:34 +0200

    Fix update frequency bug, use ConvTranspose

--- main.py
+++ main.py
@@ -14,55 +14,38 @@ import torch.optim as optim
 from board import Board
 import player
 
-NET_PLAYER = 1
-OTHER_PLAYER = 2
+MODEL = 1
+OPPONENT = 2
 
 
-class Net(nn.Module):
-    def __init__(self, board_side, hidden_layer=512):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(board_side ** 2 * 3, hidden_layer)
-        self.fc2 = nn.Linear(hidden_layer, board_side ** 2)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.softmax(x, 0)
+def win_info_to_string(win_info):
+    win_info = np.array(win_info)
+    return "model won %d/%d (%.2f), tie: %d, lost %d" % (
+        (win_info == MODEL).sum(), win_info.size,
+        (win_info == MODEL).mean(),
+        (win_info == 0).sum(),
+        (win_info == OPPONENT).sum())
 
 
 class ConvNet(nn.Module):
-    @staticmethod
-    def valid_size(in_size, field, stride):
-        ''' Calculates conv otput for given input size, field size and stride. '''
-        return math.ceil((in_size - field + 1) / stride)
-
-    def __init__(self, board_side, conv1=(32, 2, 1), conv2=(64, 2, 2), fc1=256):
+    def __init__(self, board_side):
         super(ConvNet, self).__init__()
-        self.pre_fc_side = board_side
-        self.conv1 = nn.Conv2d(
-            3, conv1[0], kernel_size=conv1[1], stride=conv1[2])
-        self.pre_fc_side = ConvNet.valid_size(self.pre_fc_side, *conv1[1:])
-        self.conv2 = nn.Conv2d(
-            conv1[0], conv2[0], kernel_size=conv2[1], stride=conv2[2])
-        self.pre_fc_side = ConvNet.valid_size(self.pre_fc_side, *conv2[1:])
-        self.fc1 = nn.Linear(self.pre_fc_side ** 2 * conv2[0], fc1)
-        self.fc2 = nn.Linear(fc1, board_side ** 2)
-        print(self)
+        self.conv1 = nn.Conv2d(3, 32, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.convTrans = nn.ConvTranspose2d(64, 1, 4)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
-        x = x.view(-1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.softmax(x, 0)
+        x = self.convTrans(x).exp()
+        return (x / x.sum()).view(-1)
 
 
 def play_game(model, opponent, net_plays_next, args, device):
     """
     Plays a single game. One player is the given model,
     the other depends on the args.
-    Returns (winner, model_move_outputs)
+    Returns (winner, chosen_move_probabilities)
     """
     board = Board(args.board_side)
     move_outputs = []
@@ -80,10 +63,10 @@ def play_game(model, opponent, net_plays_next, args, device):
             move = np.random.choice(probs.size, p=probs)
             move_outputs.append(output[move])
             move = move // args.board_side, move % args.board_side
-            board.place_move(move[0], move[1], NET_PLAYER)
+            board.place_move(move[0], move[1], MODEL)
         else:
-            move = opponent(OTHER_PLAYER, board)
-            board.place_move(move[0], move[1], OTHER_PLAYER)
+            move = opponent(OPPONENT, board)
+            board.place_move(move[0], move[1], OPPONENT)
 
         net_plays_next = not net_plays_next
         winner = board.check_winner(args.win_row, move[0], move[1])
@@ -93,37 +76,32 @@ def play_game(model, opponent, net_plays_next, args, device):
             return winner, move_outputs
 
 
-def train(args, model, device, optimizer):
+def update(args, model, device, optimizer):
     model.train()
 
     winners = []
-    t0 = time()
-    move_counts = []
+    episodes = []
     opponent = player.for_name(args.opponent)
     for game_ind in range(args.episodes_per_update):
-        winner, move_outputs = play_game(
+        winner, chosen_move_probs = play_game(
             model, opponent, bool(game_ind % 2), args, device)
         winners.append(winner)
-        move_counts.append(len(move_outputs) * 2)
+        episodes.append(chosen_move_probs)
 
-        # Calculate losses and apply gradient
-        model.zero_grad()
-        for move_output in move_outputs:
-            if not winner == NET_PLAYER:
-                move_output = 1 - move_output
-            loss = -move_output.log()
+    # Calculate losses and apply gradient
+    model.zero_grad()
+    total_loss = 0
+    for winner, chosen_move_probs in zip(winners, episodes):
+        for chosen_move_prob in chosen_move_probs:
+            prob = chosen_move_prob
+            if winner != MODEL:
+                prob = 1 - chosen_move_prob
+            loss = -prob.log()
             loss.backward()
+            total_loss += loss.detach().numpy()
+    optimizer.step()
 
-        optimizer.step()
-
-    logging.info("Played %d games in %.2f secs, average %.2f moves",
-                 args.episodes_per_update, time() - t0, np.mean(move_counts))
-    winners = np.array(winners)
-    logging.info("Net won %d (%.2f), tie: %d, lost %d",
-                 (winners == NET_PLAYER).sum(),
-                 (winners == NET_PLAYER).mean(),
-                 (winners == 0).sum(),
-                 (winners == OTHER_PLAYER).sum())
+    return winners, total_loss
 
 
 def evaluate(args, model, device):
@@ -132,15 +110,11 @@ def evaluate(args, model, device):
             logging.info("Evaluating against '%s':", name)
             winners = []
             for game_ind in range(args.eval_episodes):
-                winner, _ = play_game(model, opponent, bool(game_ind % 2), args, device)
+                winner, _ = play_game(
+                    model, opponent, bool(game_ind % 2), args, device)
                 winners.append(winner)
 
-            winners = np.array(winners)
-            logging.info("Net won %d (%.2f), tie: %d, lost %d",
-                         (winners == NET_PLAYER).sum(),
-                         (winners == NET_PLAYER).mean(),
-                         (winners == 0).sum(),
-                         (winners == OTHER_PLAYER).sum())
+            logging.info(win_info_to_string(winners))
 
 
 def parse_args():
@@ -149,18 +123,18 @@ def parse_args():
                         help='number of tiles on one side of the board')
     parser.add_argument('--win-row', type=int, default=3,
                         help='number of same-in-a-row for win')
-    parser.add_argument('--opponent', default='random',
+    parser.add_argument('--opponent', default='mixed',
                         help='the opponent used during training')
 
-    parser.add_argument('--episodes-per-update', type=int, default=128, metavar='N',
+    parser.add_argument('--episodes-per-update', type=int, default=32, metavar='N',
                         help='number of games to play per model update')
     parser.add_argument('--updates', type=int, default=100, metavar='N',
                         help='the number of net updates to perform')
-    parser.add_argument('--eval-episodes', type=int, default=256,
+    parser.add_argument('--eval-episodes', type=int, default=512,
                         help='how many episodes are played in evaluation')
 
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
+    parser.add_argument('--lr', type=float, default=0.003,
+                        help='learning rate')
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
 
@@ -175,11 +149,24 @@ def main():
     device = torch.device("cuda" if use_cuda else "cpu")
 
     model = ConvNet(args.board_side).to(device)
-    # optimizer = optim.SGD(model.parameters(), lr=args.lr)
-    optimizer = optim.RMSprop(model.parameters(), lr=1e-3)
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.5)
+    # optimizer = optim.RMSprop(model.parameters(), lr=1e-3)
 
-    for _ in range(args.updates):
-        train(args, model, device, optimizer)
+    logging.info("Eval : ---------------")
+    evaluate(args, model, device)
+
+    logging.info("Train : ---------------")
+    t0 = time()
+    for update_ind in range(args.updates):
+        t1 = time()
+        win_info, total_loss = update(args, model, device, optimizer)
+        logging.info("Update %d/%d: %d episodes in %.2f secs, %s, loss: %.2f",
+                     update_ind, args.updates,
+                     args.episodes_per_update, time() - t1,
+                     win_info_to_string(win_info), total_loss)
+    logging.info("Total training time: %.2fsec", time() - t0)
+
+    logging.info("Eval : ---------------")
     evaluate(args, model, device)
 
 
